a36ac61ca6eff6081a7df1500038e784eab37075
peterhangg <peterhanggg@gmail.com>
2019-09-26 19:03:00 +0000

    refactor

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,17 @@
 const request = require('request');
-let arg = process.argv.slice(2);
-let breedType = arg[0];
 
-const fetchBreed = function(breedType, callback) {
-  request.get(`https://api.thecatapi.com/v1/breeds/search/?q=${breedType}`, (error, response, body) => {
+const fetchBreedDescription = function(breedName, callback) {
+  request.get(`https://api.thecatapi.com/v1/breeds/search/?q=${breedName}`, (error, response, body) => {
     if (error) {
-      console.log((error, null));
+      callback(null, error);
     }
     const breedInfo = JSON.parse(body)[0];
     if (breedInfo) {
-      console.log(breedInfo.description);
+      callback(null, breedInfo.description.trim());
     } else {
-      console.log("this breed doesn't exist!");
+      callback(null, "this breed doesn't exist!");
     }
   });
 };
 
-fetchBreed(breedType);
+module.exports = { fetchBreedDescription };
